65bcbccc4d82a0805c7581286faf4b793689fa1c
TemirlanIdrissov <51860679+TemirlanIdrissov@users.noreply.github.com>
2019-07-01 20:49:05 +0600

    Add files via upload

---
+++ size.rb
@@ -0,0 +1,17 @@
+def size(words)
+  mas = []
+
+  words.each do |s| 
+    mas << s.length.to_i
+  end
+    mas.sort!
+
+    puts "Min: #{mas[0]}"
+    puts "Max #{mas[-1]}"
+    puts "Total: #{mas[0] + mas[-1]}"
+
+end
+
+str = ['apple','orange','banana','book']
+
+size(str)
